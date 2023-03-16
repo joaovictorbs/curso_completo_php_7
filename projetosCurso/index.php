d11ea91ba074ac0df624aa8ab322182e1049d331
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2023-03-16 20:19:04 -0300

    Variaveis

--- projetosCurso/index.php
+++ projetosCurso/index.php
@@ -1,8 +1,28 @@
 <?php
+//                              comentario de linha
+/*                              comentario de blocos  */
 
-$nome = 'Hcode';    //string
+$anoNascimento = 2002;          //camelCase
+$nome = 'João';
+$sobrenome = 'Claudio'; 
+//$1nome = '';                  erro por conta do numero no inicio
+$_nome = '';                    //underline é um caractere especial permitido
+//$this                         variavel interna, nao pode ser usada para simples associacao
 
-// echo $nome;
-var_dump($nome);
+$nomeCompleto = $nome . " " . $sobrenome;       //usamos . para concatenacao 
+echo $nomeCompleto;
+echo "<br />";
+// exit;                        paralisa execucao do php
+
+
+$nomeFinal = 'Marcelo';
+echo $nomeFinal;
+echo "<br />";                  //quebra linha
+
+// unset($nomeFinal, $nome1);   retira variavel da memoria
+
+if (isset($nomeFinal)) {         //verifica se variavel existe
+    echo $nomeFinal;
+}
 
 ?>
