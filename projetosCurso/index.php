3783883815aaee2f9b9405fccd22a686d8f40711
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2023-04-10 23:28:20 -0300

    Incluindo arquivos com include e require

--- projetosCurso/index.php
+++ projetosCurso/index.php
@@ -1,39 +1,14 @@
 <?php
 
-$nome = "Hcode";
-$nome2 = 'Treinamentoos';
+//include "../inc/index.php";       //como se fosse um arquivo em outra pasta 
+//include "index.php";              //chamamos o arquivo que queremos utilizar a funcao
+//include "index.php".$_GET[""];    //permite manipulacao de arquivo malicioso
+//require "index.php";              //chamamos o arquivo que queremos utilizar a funcao || require verifica se arquivo existe e se funciona antes
+//include_once "index.php";         //chama arquivo somente uma vez, evitando chamar duas vezes
+require_once "index.php";           //verifica se nao esta chamando arquivos que ja tem um require sendo utilizado dentro dele, evitando chamar duas vezes
 
-echo "ABC $nome"; echo "<br>";
-echo 'ABC $nome'; echo "<br>";
+$resultado = somar(10, 20);         //chama funcao de outro arquivo para somar
 
-
-$nome = "JOAO barbosa";
-echo strtoupper($nome);echo "<br>"; //funcao que deixa strings em maisculo
-echo strtolower($nome);echo "<br>"; //funcao que deixa strings em minusculo
-
-echo ucfirst($nome);echo "<br>";    //deixa primeira letra de cada palavra maiscula
-echo ucwords($nome);echo "<br>";    //deixa somente a primeira palavra em maiscula
-
-
-$empresa = "Hcode";
-$empresa = str_replace("o", "0", $empresa);  //troca uma informacao por outra | informamos o que queremos trocar, pelo que, e qual variável
-$empresa = str_replace("e", "3", $empresa);  
-echo $empresa;echo "<br>";
-
-
-$frase = "A repetição é a mãe da retenção";
-$palavra = "mãe";
-$q = strpos($frase, "mãe");                 //buscamos o indice de uma informacao dentro da variavel 
-var_dump($q);
-
-echo "<br>";
-
-$texto = substr($frase, 0, $q);             //comeca a capturar informacao a partir de determinado indice
-var_dump($texto);
-
-echo "<br>";
-
-$texto2 = substr($frase, $q + strlen($palavra), strlen($frase));
-var_dump($texto2);
+echo $resultado;
 
 ?>
