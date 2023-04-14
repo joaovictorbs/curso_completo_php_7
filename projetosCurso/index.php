9566545257de301707aa77be6487588ae2bb5fc0
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2023-04-13 21:36:07 -0300

    Estruturas condicionais - if, elseif, else

--- projetosCurso/index.php
+++ projetosCurso/index.php
@@ -1,7 +1,26 @@
 <?php
 
-function somar($a, $b){
-    return $a + $b;
+$qualSuaIdade = 30;
+
+$idadeCrianca = 12;
+$idadeMaior = 18;
+$idadeMelhor = 65;
+
+if($qualSuaIdade < $idadeCrianca){       // se for true
+    echo "Criança";
 }
+elseif($qualSuaIdade < $idadeMaior) {    // se a primeira regra nao for true                       
+    echo "Adolescente";
+}
+elseif($qualSuaIdade < $idadeMelhor){
+    echo "Adulto";
+}
+else {                                   // se nao for true
+    echo "Idoso";
+}
+
+echo "<br>";
+
+echo ($qualSuaIdade < $idadeMaior) ? "Menor de Idade" : "Maior de Idade"; //operador ternario
 
 ?>
