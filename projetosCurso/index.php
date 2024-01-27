2ea4fb3ad763a8701aa1fa95c50877d85011dc80
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2024-01-26 22:45:23 -0300

    Foreach

--- projetosCurso/index.php
+++ projetosCurso/index.php
@@ -1,29 +1,38 @@
 <?php
 
-for ($i = 0; $i <= 10; $i++) { #declara valor de $i, informa a condição de repeticao e informa o incremento
-    echo $i . "<br>";
+$meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+foreach($meses as $mes) {  #para cada mes no array meses
+    echo $mes . "<br>";
 }
 
 echo "<br><br>";
 
-for ($i = 0; $i <= 1000; $i+=5) { #vai somando 5
-    
-    if ($i > 200 && $i < 800) continue; #entre 200 e 800 pula / caso if tenha somente uma instrucao, nao precisa do {}
-
-
-    if ($i === 900) break; #valida valor e tipo de valor
-
-    echo $i . "<br>";
+foreach($meses as $index => $mes) {  #indice do array
+    echo "Indice " . $index . "<br>";
+    echo "Mes " . $mes . "<br>";
 }
 
 echo "<br><br>";
+?>
 
-echo "<select>";
+<form>
 
-for ($i=date("Y"); $i > date("Y")-100; $i--) {
-    echo '<option value="'.$i.'">'.$i.'</option>';
+    <input type="text" name="nome">
+    <input type="date" name="nascimento">
+    <input type="submit" name="Ok">
+
+</form>
+
+<?php
+
+if (isset($_GET)) {                         #se for um formulario muito grande, cria foreach
+    foreach ($_GET as $key => $value) {
+        echo "Nome do campo: " .$key;
+        echo "<br>";
+        echo "Valor do campo: " .$value;
+
+        echo "<hr>";
+    }
 }
-
-echo "</select>"
-
 ?>
