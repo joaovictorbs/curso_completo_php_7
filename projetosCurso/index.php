09e030467e795348d280dd138c8c94e602cfe4b5
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2024-01-26 22:30:11 -0300

    For

--- projetosCurso/index.php
+++ projetosCurso/index.php
@@ -1,39 +1,29 @@
 <?php
 
-$diaSemana = date("w");
-
-switch ($diaSemana) {
-    case 0:
-        echo "Domingo";
-        break;  #interrompe o switch
-    
-    case 1:
-        echo "Segunda-Feira";
-        break;
-    
-    case 2:
-        echo "Terça-Feira";
-        break;
-    
-    case 3:
-        echo "Quarta-Feira";
-        break;
-    
-    case 4:
-        echo "Quinta-Feira";
-        break;
-    
-    case 5:
-        echo "Sexta-Feira";
-        break;
-    
-    case 6:
-        echo "Sábado";
-        break;
-
-    default:    #caso nao caia em nenhum caso, entra no default
-        echo "Data inválida!";
-        break;
+for ($i = 0; $i <= 10; $i++) { #declara valor de $i, informa a condição de repeticao e informa o incremento
+    echo $i . "<br>";
 }
+
+echo "<br><br>";
+
+for ($i = 0; $i <= 1000; $i+=5) { #vai somando 5
+    
+    if ($i > 200 && $i < 800) continue; #entre 200 e 800 pula / caso if tenha somente uma instrucao, nao precisa do {}
+
+
+    if ($i === 900) break; #valida valor e tipo de valor
+
+    echo $i . "<br>";
+}
+
+echo "<br><br>";
+
+echo "<select>";
+
+for ($i=date("Y"); $i > date("Y")-100; $i--) {
+    echo '<option value="'.$i.'">'.$i.'</option>';
+}
+
+echo "</select>"
 
 ?>
