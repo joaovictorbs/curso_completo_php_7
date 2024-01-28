ba63ac1f8967e627b8cd9c048a55f20aed68c1e8
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2024-01-28 17:28:00 -0300

    Teste sessoes

--- projetosCurso/index.php
+++ projetosCurso/index.php
@@ -1,14 +1,8 @@
 <?php
 
-session_id('7u0d01a1n81s2sneu893nvhust'); # define id de sessao fixo
-
 session_start();
 
-echo session_id(); # funcao id de sessao
+echo session_save_path();
 
-echo "<br>";
 
-session_regenerate_id(); # gera um novo id de sessao
-
-echo session_id();
 ?>
