9717ca1784b23f926896876dd6d741d887bc1a30
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2024-01-28 18:11:40 -0300

    Parametros por valor X parametros por referencia

--- projetosCurso/index.php
+++ projetosCurso/index.php
@@ -1,18 +1,40 @@
 <?php
 
-function ola($texto, $periodo = "Bom dia") {    # define parametro obrigatorio / define valor padrao
-    return "$periodo<br>Olá $texto!<br><br>";
+$a = 10;
+
+function trocaValor($a) {
+    $a += 50;
+
+    return $a;
 }
 
-function teste() {
-    return func_get_args(); # exibe os parametros recebidos
+echo $a. "<br>";
+echo trocaValor($a). "<br>";
+echo $a. "<br>"; # o que acontece na funcao fica na funcao, o nome da variavel não significa que é a mesma da funcao
+
+echo "<br><br>";
+
+$b = 10;
+
+function trocaValorReferencia(&$b) { # parametro por referencia
+    $b += 50;
+
+    return $b;
 }
 
-echo ola("teste");
-echo ola("João", "Boa noite");
-echo ola("teste");
+echo $b. "<br>";
+echo trocaValorReferencia($b). "<br>";
+echo $b. "<br>"; # o valor altera pois passou o valor por referencia
+echo trocaValorReferencia($b). "<br>";
+echo $b. "<br>"; 
 
-var_dump(teste("texto", 123));
+echo "<br><br>";
 
+$pessoa = ['nome' => 'João', 'idade' => 20];
+
+foreach ($pessoa as &$value) {  # o valor passa por referencia
+    if (gettype($value) === 'integer') $value += 10;    # gettype verifica tipo do valor
+    echo $value. "<br>";
+}
 
 ?>
