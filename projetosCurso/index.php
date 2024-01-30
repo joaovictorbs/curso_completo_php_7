b3753e5d7566c158308a2c4a4ef430f3cd974361
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2024-01-30 16:21:32 -0300

    Interface

--- projetosCurso/index.php
+++ projetosCurso/index.php
@@ -1,65 +1,28 @@
 <?php
 
-class Documento {
-    private $numero;
-    
-    public function getNumero() {
-        return $this->numero;
+interface Veiculo { # cria interface
+    public function acelerar($velocidade); # precisa ter metodo acelerar 
+    public function frear($velocidade); 
+    public function trocarMarcha($marcha); 
+}
+
+class Civic implements Veiculo { # a classe precisa implementar a interface veiculo
+    public function acelerar($velocidade) # o nome da variavel nao precisa ser o mesmo, somente o do metodo
+    {
+        echo "O veículo acelerou até ". $velocidade . " km/h";
     }
     
-    public function setNumero($n) {
-        $this->numero = $n;
+    public function frear($velocidade)
+    {
+        echo "O veículo freou até ". $velocidade . " km/h";
+    }
+    
+    public function trocarMarcha($marcha)
+    {
+        echo "O veículo engatou para a marcha ". $marcha; 
     }
 }
 
-class CPF extends Documento {  # heranca
-    public function validaCpf():bool {
-        $cpf = $this->getNumero();
-
-        //validacao do CPF
-        if(empty($cpf)) {
-            return false;
-        }
-        
-        $cpf = preg_match('/[0-9]/', $cpf)?$cpf:0;
-        
-        $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
-        
-        if (strlen($cpf) != 11) {
-            echo "length";
-            return false;
-        }
-        else if ($cpf == '00000000000' || 
-            $cpf == '11111111111' || 
-            $cpf == '22222222222' || 
-            $cpf == '33333333333' || 
-            $cpf == '44444444444' || 
-            $cpf == '55555555555' || 
-            $cpf == '66666666666' || 
-            $cpf == '77777777777' || 
-            $cpf == '88888888888' || 
-            $cpf == '99999999999') {
-            return false;
-        }
-        else {   
-            for ($t = 9; $t < 11; $t++) {
-                for ($d = 0, $c = 0; $c < $t; $c++) {
-                    $d += $cpf[$c] * (($t + 1) - $c);
-                }
-                $d = ((10 * $d) % 11) % 10;
-                if ($cpf[$c] != $d) {
-                    return false;
-                }
-            }
-        
-            return true;
-        }
-    }
-}
-
-$doc = new CPF();
-
-$doc->setNumero("427937900");
-var_dump($doc->validaCpf());
-
+$carro = new Civic;
+$carro->trocarMarcha("3");
 ?>
