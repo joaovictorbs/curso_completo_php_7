a1c48acc918b39160fc05507f7ac5cb87d2295ba
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2024-01-30 12:14:35 -0300

    Atributos e Metodos

--- projetosCurso/index.php
+++ projetosCurso/index.php
@@ -1,18 +1,55 @@
 <?php
 
-class Pessoa {
+class Carro {
 
-    public $nome; # atributo
+    private $modelo; # atributo privado
+    
+    private $motor;
+    
+    private $ano;
 
-    public function falar() { # metodo
-        return "Meu nome é " . $this->nome; # acessa atributo dentro de classe
+    public function getModelo() { # recupera atributo modelo
+        return $this->modelo;
+    }
+    
+    public function setModelo($modelo) { # define informacao atributo modelo
+        $this->modelo = $modelo;
+    }
+
+    public function getMotor():float { # define tipo float
+        return $this->motor;
+    }
+    
+    public function setMotor($motor) {
+        $this->motor = $motor;
+    }
+
+    public function getAno():int { #define tipo int
+        return $this->ano;
+    }
+    
+    public function setAno($ano) {
+        $this->ano = $ano;
+    }
+
+
+
+    public function exibe() { # metodo que retorna array de informacoes
+        return [
+                "modelo"=>$this->getModelo(), 
+                "motor"=>$this->getMotor(),
+                "ano"=>$this->getAno()
+            ];
     }
 }
 
 
 
-$joao = new Pessoa;
-$joao->nome = "João"; # associa nome a atributo que é publico
-echo $joao->falar();
+$uno = new Carro;
+$uno->setModelo("Fiat"); # associa dado ao atributo pelo metodo, já que o atributo é privado
+$uno->setMotor(1.0);
+$uno->setAno(1983);
+
+var_dump($uno->exibe());
 
 ?>
