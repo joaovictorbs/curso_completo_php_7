645813fc4643f7e3a7d3ee2f96567742c1c13ba2
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2024-01-31 17:15:15 -0300

    Classe MySQLi

--- projetosCurso/index.php
+++ projetosCurso/index.php
@@ -1,16 +1,38 @@
 <?php
 
-require_once("config.php");
+$conn = new mysqli("localhost", "root", "", "dbphp7"); # classe banco mysql / passa servidor, usuario, senha e banco
 
-# usa a classe cadastro dentro da pasta cliente
-use Cliente\Cadastro;
+if ($conn->connect_error) { # verifica se ocorreu algum erro na conexao
+    echo "Error " . $conn->connect_error;
+} 
 
-$cad = new Cadastro;
+# INSERT DE TESTE
+/*
+$stmt = $conn->prepare("INSERT INTO tb_usuarios (usu_login, usu_senha) VALUES (?, ?)"); # prepara comando que queremos enviar no banco / ? é para receber os valores
 
-$cad->setNome("João");
-$cad->setEmail("joao@teste.com.br");
-$cad->setSenha("123456");
+$stmt->bind_param("ss", $login, $pass); # espera os tipos de dados do ? / s de string para o primeiro e segundo
 
-$cad->registraVenda();
+$login  = "user";
+$pass   = "12345";
+
+$stmt->execute(); # realiza o comando 
+
+$login  = "root";
+$pass   = "testeSenha";
+
+$stmt->execute(); # realiza outro insert
+*/
+
+$result = $conn->query("SELECT * FROM tb_usuarios ORDER BY usu_login"); # realiza comando direto no banco
+
+$data = array();
+
+// while ($row = $result->fetch_array()) { # retorna dados se existir / o while é enquanto tem resultado armazena em row
+while ($row = $result->fetch_assoc()) { # assoc retorna somente os valores do array
+    array_push($data, $row);   # array_push adiciona dados no array
+}
+
+echo json_encode($data); # transforma em JSON
+
 
 ?>
