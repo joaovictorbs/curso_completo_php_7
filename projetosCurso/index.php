de2a714f3154688361037aa86328e776725833f9
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2024-01-31 20:35:57 -0300

    Deletando dados PDO

--- projetosCurso/index.php
+++ projetosCurso/index.php
@@ -2,16 +2,11 @@
 
 $conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root", "");
 
-$stmt = $conn->prepare("UPDATE tb_usuarios SET usu_login = :login, usu_senha = :password WHERE usu_id = :id");
-$login      = 'Almir';
-$password   = '0987654321';
-$id         = 3;
+$stmt = $conn->prepare("DELETE FROM tb_usuarios WHERE usu_id = :id");
+$id   = 2;
 
-$stmt->bindParam(":login", $login);
-$stmt->bindParam(":password", $password);
 $stmt->bindParam(":id", $id);
 
 $stmt->execute();
-
 
 ?>
