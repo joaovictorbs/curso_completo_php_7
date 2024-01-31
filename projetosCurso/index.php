f7d4b0e95f6a8a096b89e36b3d2e489ebd3d91b5
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2024-01-31 20:33:25 -0300

    Alterando dados PDO

--- projetosCurso/index.php
+++ projetosCurso/index.php
@@ -2,13 +2,16 @@
 
 $conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root", "");
 
-$stmt = $conn->prepare("INSERT INTO tb_usuarios (usu_login, usu_senha) VALUES (:login, :password) "); # PDO reconhece parametros
-$login = 'José';
-$password = '1234567890';
+$stmt = $conn->prepare("UPDATE tb_usuarios SET usu_login = :login, usu_senha = :password WHERE usu_id = :id");
+$login      = 'Almir';
+$password   = '0987654321';
+$id         = 3;
 
 $stmt->bindParam(":login", $login);
 $stmt->bindParam(":password", $password);
+$stmt->bindParam(":id", $id);
 
 $stmt->execute();
+
 
 ?>
