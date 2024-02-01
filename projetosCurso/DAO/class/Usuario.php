aa89d0ab038a172e6515b7bbc42aa767bd5356b9
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2024-02-01 16:26:08 -0300

    DAO - INSERT | UPDATE | DELETE

--- projetosCurso/DAO/class/Usuario.php
+++ projetosCurso/DAO/class/Usuario.php
@@ -7,6 +7,12 @@ class Usuario {
     private $usu_senha;
     private $usu_dt_cadastro;
 
+    public function __construct($login = "", $password = "")
+    {
+        $this->setUsuLogin($login);
+        $this->setUsuSenha($password);
+    }
+
     public function __toString()
     {
         return json_encode(array(
@@ -49,6 +55,14 @@ class Usuario {
         $this->usu_dt_cadastro = $value;
     }
 
+    public function setData($data) {
+        $this->setUsuId($data['usu_id']);
+        $this->setUsuLogin($data['usu_login']);
+        $this->setUsuSenha($data['usu_senha']);
+        $this->setUsuDtCadastro(new DateTime($data['usu_dt_cadastro'])); # formata data
+    }
+
+
     public function loadById($id) { # carrega usuario
         $sql = new Sql();
 
@@ -60,19 +74,17 @@ class Usuario {
         if (count($result) > 0) {
             $row = $result[0];
 
-            $this->setUsuId($row['usu_id']);
-            $this->setUsuLogin($row['usu_login']);
-            $this->setUsuSenha($row['usu_senha']);
-            $this->setUsuDtCadastro(new DateTime($row['usu_dt_cadastro'])); # formata data
-
+            $this->setData($row);
         }
     }
+
 
     public static function getList() { # metodo pode ser estatico pois nao tem associacao ao proprio objeto
         $sql = new Sql();
 
         return $sql->select("SELECT * FROM tb_usuarios ORDER BY usu_login");
     }
+
 
     public static function search($login) {
         $sql = new Sql();
@@ -81,6 +93,7 @@ class Usuario {
             ':login'=>"%$login%"
         ));
     }
+
 
     public function login($login, $password) {
         $sql = new Sql();
@@ -94,15 +107,57 @@ class Usuario {
         if (count($result) > 0) {
             $row = $result[0];
 
-            $this->setUsuId($row['usu_id']);
-            $this->setUsuLogin($row['usu_login']);
-            $this->setUsuSenha($row['usu_senha']);
-            $this->setUsuDtCadastro(new DateTime($row['usu_dt_cadastro'])); # formata data
-
+            $this->setData($row);
         }
         else {
             throw new Exception("Login e/ou senhas inválidos", 1);
         }
+    }
+
+
+    public function insert() {  # insere novo cadastro
+        $sql = new Sql();
+        
+        $result = $sql->select("CALL sp_usuarios_insert(:login, :password)", array(
+            ":login"=>$this->getUsuLogin(),
+            ":password"=>$this->getUsuSenha()
+        )); # cria procedimento
+
+        if (count($result) > 0) {
+            $row = $result[0];
+
+            $this->setData($row);
+        }
+    }
+
+
+    public function update($login, $password) {  # atualiza registro
+        
+        $this->setUsuLogin($login);
+        $this->setUsuSenha($password);
+
+        $sql = new Sql();
+        
+        $sql->runQuery("UPDATE tb_usuarios SET usu_login = :login, usu_senha = :password WHERE usu_id = :id", array(
+            ":login"=>$this->getUsuLogin(),
+            ":password"=>$this->getUsuSenha(),
+            ":id"=>$this->getUsuId()
+        ));
+    }
+
+
+    public function delete() {  # deleta registro
+        
+        $sql = new Sql();
+        
+        $sql->runQuery("DELETE FROM tb_usuarios WHERE usu_id = :id", array(
+            ":id"=>$this->getUsuId()
+        ));
+
+        $this->setUsuId(0);
+        $this->setUsuLogin("");
+        $this->setUsuSenha("");
+        $this->setUsuDtCadastro(new DateTime()); # limpa os dados do usuario
     }
 
 
--- projetosCurso/DAO/index.php
+++ projetosCurso/DAO/index.php
@@ -2,24 +2,11 @@
 
 require_once("config.php");
 
-$usuario = new Usuario;
+$usuario = new Usuario(); # metodo construtor aplica valores aos atributos
 
-# busca um usuarios
-// $usuario->loadById(3);
+$usuario->loadById(7);  # recupera dados do usuario
 
-# valida login
-$usuario->login("user", "12345");
+$usuario->delete();
 
-# busca lista de usuarios
-$lista = Usuario::getList();
-
-# busca lista de usuarios por login
-$listaLogin = Usuario::search('user');
-
-
-echo $usuario; # chama o toString
-echo "<br><br>";
-echo json_encode($lista); # retorna lista de usuarios
-echo "<br><br>";
-echo json_encode($listaLogin); # retorna lista de usuarios pelo login
+echo $usuario;
 ?>
