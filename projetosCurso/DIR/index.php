54f2df051927d0f54df54d97e7e704a75d3d0b02
Joao Victor Barbosa Santos <joaovictorjvbs12@gmail.com>
2024-02-02 14:07:45 -0300

    Lendo conteudo em arquivos

--- projetosCurso/DIR/index.php
+++ projetosCurso/DIR/index.php
@@ -1,15 +1,46 @@
 <?php
 
-require_once("config.php");
+$filename = "usuarios.csv";
 
-if (!is_dir("imagesTeste")) mkdir("imagesTeste");
+if(file_exists($filename)) { # verifica existencia do arquivo
+    $file = fopen($filename, "r"); # informa leitura
+  
+    $headers = explode(",", fgets($file)); # captura linha por linha
+  
+    $data = array();
 
-foreach (scandir("imagesTeste") as $item) {
-    if(!in_array($item, array(".", ".."))) {
-        unlink("imagesTeste/" . $item);
+    while ($row = fgets($file)) { # verifica se existem mais linhas
+        
+        $rowData = explode(",", $row);
+        $linha = array();
+
+        for ($i = 0; $i < count($headers); $i++) { # verifica cada coluna do header
+            $linha[$headers[$i]] = $rowData[$i]; # busca pelo valor associando a chave que é o header
+        }
+        
+        array_push($data, $linha); # array chave e valor
+
     }
+
+    fclose($file);
+
+    echo json_encode($data);
 }
 
-echo "Arquivos removidos com sucesso!";
+
+$filename = "images/teste.png";
+
+$base64 = base64_encode(file_get_contents($filename)); # pega informacao do arquivo e codifica em base64
+
+$fileinfo = new finfo(FILEINFO_MIME_TYPE); # recupera tipo de arquivo
+
+$mimetype = $fileinfo->file($filename);
+
+$base64encode = "data:" . $mimetype . ";base64," . $base64;
 
 ?>
+<br><br>
+<a href="<?=$base64encode?>" target="_blank"> Link Para Imagem </a>
+<br><br>
+<img src="<?=$base64encode?>">
+<br><br>
